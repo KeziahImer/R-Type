4e8798fd71b432ebd3ac4f20ebcedaf513dcab9a
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-09-26 10:58:56 +0200

    :poop: add a basic main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello, World! I'm the client" << std::endl;
+    sf::RenderWindow _window(sf::VideoMode(1920, 1080), "R-Type");
+    while (_window.isOpen())
+    {
+        sf::Event event;
+        if (_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                _window.close();
+        }
+        _window.clear();
+        _window.display();
+    }
     return 0;
 }
