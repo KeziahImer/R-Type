bd445c63a9f60537bb20111ba10694ab128e2e9d
Léandre <leandre.sentenac@epitech.eu>
2023-09-27 11:03:24 +0200

    🔥 add assets

--- client/main.cpp
+++ client/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 #include "GameEngine/ECS.hpp"
+#include "components/Components.hpp"
 
 int main()
 {
+    GameEngine::Registry registry;
+    GameEngine::EntityID entity = registry.createEntity("player");
+
+    registry.addComponent<Position>(entity, Position(40, 50));
+    registry.addComponent<Size>(entity, Size(100, 50));
+    std::map<sf::Keyboard::Key, std::pair<int, int>> keybinds{{sf::Keyboard::Z, std::make_pair<int, int>(0, -5)}, {sf::Keyboard::Q, std::make_pair<int, int>(-5, 0)}, {sf::Keyboard::S, std::make_pair<int, int>(0, 5)}, {sf::Keyboard::D, std::make_pair<int, int>(5, 0)}};
+    registry.addComponent<Movable>(entity, Movable(keybinds));
+    registry.addComponent<Velocity>(entity, Velocity(0, 0));
+    registry.addComponent<Sprite>(entity, Sprite("/assets/Player.gif", 33, 17, 0, 2));
     // Renderer renderer;
     // Inputer inputs;
     // GameEngine engine;
@@ -11,7 +21,7 @@ int main()
     // SceneManager sceneManager;
     // get scenes
 
-    bool playing = true;
+    bool playing = false;
     while (playing)
     {
         // inputs.getInputs();
@@ -19,18 +29,5 @@ int main()
         // registry.applySystems();
         // renderer.render();
     }
-    GameEngine::Registry registry;
-
-    GameEngine::EntityID entity = registry.createEntity("player");
-
-    GameEngine::SparseArray<int> &components = registry.registerComponent<int>();
-    registry.addComponent<int>(17, 42);
-
-    std::cout << "Components: " << std::endl;
-    for (size_t i = 0; i < components.size(); i++)
-    {
-        std::cout << components[i] << std::endl;
-    }
-
     return 0;
 }
