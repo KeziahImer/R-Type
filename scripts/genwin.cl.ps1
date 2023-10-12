a1dabb58e5f24432f296f09011c672633fa38c77
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-10-12 10:13:03 +0200

    :hammer: update the genwin scripts

--- scripts/genwin.cl.ps1
+++ scripts/genwin.cl.ps1
@@ -1,6 +1,6 @@
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-Set-Location -Path $scriptDir
+Set-Location -Path $scriptDir\..
 
 cmake -B ./build/win/cl -DCMAKE_CXX_COMPILER=cl -DCMAKE_C_COMPILER=cl -DCMAKE_BUILD_TYPE=Release -S ./
 
