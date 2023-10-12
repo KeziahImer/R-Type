faf4471d5acb8a1f92b503b40820588c2beda724
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-10-12 10:15:16 +0200

    :sparkles: add scenes

---
+++ client/include/rngine/Scene.hpp
@@ -0,0 +1,121 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** Scene deifinition
+*/
+
+#ifndef _RNGINE_SCENE_HPP_
+#define _RNGINE_SCENE_HPP_
+
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "./Registry.hpp"
+
+namespace RNGine {
+/**
+ * @brief A Scene contain a registry and load it when it's created
+ */
+class Scene {
+public:
+  Scene(const std::string &id) : _id(id) {}
+
+  RNGine::Entity createEntity(std::string const &name) {
+    if (_loaded) {
+      return _registry.createEntity(name);
+    } else {
+      return _initial.createEntity(name);
+    }
+  }
+
+  RNGine::Entity getEntity(std::string const &name) {
+    if (_loaded) {
+      return _registry.getEntity(name);
+    } else {
+      return _initial.getEntity(name);
+    }
+  }
+
+  Scene &addBundle(RNGine::Registry::SystemBundle const &bundle) {
+    if (_loaded) {
+      _registry.addBundle(bundle);
+    } else {
+      _initial.addBundle(bundle);
+    }
+    return *this;
+  }
+
+  Scene &removeBundle(const size_t &index) {
+    if (_loaded) {
+      _registry.removeBundle(index);
+    } else {
+      _initial.removeBundle(index);
+    }
+    return *this;
+  }
+
+  template <typename Component>
+  Scene &addComponent(RNGine::Entity entity, Component const &component) {
+    if (_loaded) {
+      _registry.addComponent(entity, component);
+    } else {
+      _initial.addComponent(entity, component);
+    }
+    return *this;
+  }
+
+  template <typename Component> Scene &removeComponent(RNGine::Entity entity) {
+    if (_loaded) {
+      _registry.removeComponent<Component>(entity);
+    } else {
+      _initial.removeComponent<Component>(entity);
+    }
+    return *this;
+  }
+
+  Scene &removeEntity(RNGine::Entity entity) {
+    if (_loaded) {
+      _registry.removeEntity(entity);
+    } else {
+      _initial.removeEntity(entity);
+    }
+    return *this;
+  }
+
+  Scene &load() {
+    if (!_loaded) {
+      _registry = _initial;
+      _loaded = true;
+    }
+    return *this;
+  }
+
+  Scene &unload() {
+    if (_loaded) {
+      _registry = _initial;
+      _loaded = false;
+    }
+    return *this;
+  }
+
+  Scene &update() {
+    if (!_loaded)
+      throw std::runtime_error("Scene not loaded.");
+    _registry.run();
+    return *this;
+  }
+
+  std::string getId() const { return _id; }
+
+private:
+  std::string _id;
+  RNGine::Registry _registry;
+  RNGine::Registry _initial;
+  bool _loaded = false;
+};
+}; // namespace RNGine
+
+#endif
--- client/main.cpp
+++ client/main.cpp
@@ -1,51 +1,13 @@
-#include "rngine/Entity.hpp"
-#include "rngine/Registry.hpp"
-#include "rngine/SparseArray.hpp"
+
 #include <iostream>
 
+#include "rngine/Scene.hpp"
+
 int main() {
-  RNGine::Entity entity = 0;
-  RNGine::SparseArray<int> arr;
-  arr.insert_at(0, 1);
-  arr.insert_at(56, 43);
-
-  for (auto num : arr) {
-    if (num.has_value()) {
-      std::cout << *num << std::endl;
-    }
-  }
-
-  RNGine::Registry::SystemBundle bundle;
-
-  bundle.push_back([](RNGine::Registry &) {
-    std::cout << "0 Hello world from bundle" << std::endl;
-  });
-  bundle.push_back([](RNGine::Registry &) {
-    std::cout << "1 Hello world from bundle" << std::endl;
-  });
-  bundle.push_back([](RNGine::Registry &) {
-    std::cout << "2 Hello world from bundle" << std::endl;
-  });
-  bundle.push_back([](RNGine::Registry &) {
-    std::cout << "3 Hello world from bundle" << std::endl;
-  });
-
-  RNGine::Registry registry;
-
-  registry.addBundle(bundle);
-
-  registry.run();
-
-  auto test = registry.createEntity("test");
-  registry.addComponent(test, 42);
-
-  auto components = registry.getComponents<int>();
-  auto component = components[test];
-
-  std::cout << (component.has_value() ? *component : -84) << std::endl;
-  component = components[54375];
-
-  std::cout << (component.has_value() ? *component : -84) << std::endl;
+  RNGine::Scene scene("menu");
+  scene.createEntity("button-solo");
+  scene.createEntity("button-multiplayer");
+  scene.createEntity("button-settings");
 
   return 0;
 }
