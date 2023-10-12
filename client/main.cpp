75ddf84c59b77ed6d10e10b37685d67e6d3de26b
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-10-12 10:15:16 +0200

    :sparkles: add sparse array

--- client/main.cpp
+++ client/main.cpp
@@ -1,1 +1,16 @@
-int main() { return 0; }
+
+#include "rngine/SparseArray.hpp"
+#include <iostream>
+
+int main() {
+  RNGine::SparseArray<int> arr;
+  arr.insert_at(0, 1);
+  arr.insert_at(56, 43);
+
+  for (auto num : arr) {
+    if (num.has_value()) {
+      std::cout << *num << std::endl;
+    }
+  }
+  return 0;
+}
