c7377fb38cc22b2750b9ff5a46e246677c2b3175
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-10-12 10:15:16 +0200

    Add renrederer with components

---
+++ client/include/rngine/components/Position.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** Rtype
+** File description:
+** Components
+*/
+
+#ifndef _RNGINE_COMPONENTS_POSITION_HPP_
+#define _RNGINE_COMPONENTS_POSITION_HPP_
+namespace RNGine {
+namespace components {
+struct Position {
+  float x;
+  float y;
+};
+} // namespace components
+} // namespace RNGine
+#endif
---
+++ client/include/rngine/components/Size.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** Rtype
+** File description:
+** Components
+*/
+
+#ifndef _RNGINE_COMPONENTS_SIZE_HPP_
+#define _RNGINE_COMPONENTS_SIZE_HPP_
+namespace RNGine {
+namespace components {
+struct Size {
+  double scaleX;
+  double scaleY;
+};
+} // namespace components
+} // namespace RNGine
+
+#endif
