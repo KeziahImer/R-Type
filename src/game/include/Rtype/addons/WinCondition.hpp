1e61960f2ce512440f389676fc2c8cae3cd0e870
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-11-05 22:47:12 +0100

    Refactor WinCondition component and add WinConditionSystem

--- src/game/include/Rtype/addons/WinCondition.hpp
+++ src/game/include/Rtype/addons/WinCondition.hpp
@@ -5,17 +5,27 @@
 ** Rtype game
 */
 
-#ifndef _RTYPE_CLIENT_COMPONENTS_WINCONDITION_HPP_
-#define _RTYPE_CLIENT_COMPONENTS_WINCONDITION_HPP_
+#ifndef _RTYPE_CLIENT_ADDONS_WINCONDITION_HPP_
+#define _RTYPE_CLIENT_ADDONS_WINCONDITION_HPP_
 
+#include <optional>
+#include <vector>
+
+#include "RNGine/Core.hpp"
 #include "RNGine/RNGine.hpp"
+#include "RNGine/Scene.hpp"
 
-namespace Rtype {
+namespace Client {
 namespace Components {
 struct WinCondition {
-  bool winCondition = false;
+  RNGine::Core::Time endGame = 0;
+  bool boss = false;
+  RNGine::Core::Time lastUpdate = 0;
+  std::vector<RNGine::Scene::Entity> entities;
 };
+
+void WinConditionSystem(RNGine::Core &core);
 } // namespace Components
-} // namespace Rtype
+} // namespace Client
 
 #endif
